6a3e4bac3423d88155c1d917337dbeaba4f46fe5
Misha Marodi <mishamarodi@Mishas-MacBook-Pro.local>
2021-04-05 23:49:57 +0200

    cookie doesnt open if accepted

--- src/scripts/createCookieEvent.ts
+++ src/scripts/createCookieEvent.ts
@@ -2,7 +2,7 @@ import {setCookie} from "./Cookie";
 
 export const createCookieEvent = (target: HTMLElement, event: string, status: string, array: Set<number>, popup?: any) => {
     target.addEventListener(event, () => {
-        let value = `${status}; Vendors:`
+        let value = `${status}; Vendors: `
         array.forEach((item) => {
             value += `${item} `
         })
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,6 +1,15 @@
 import '../style.css'
 import {initCookiePopup} from "./initCookiePopup";
+import {getCookie} from "./Cookie";
 
 const path = "https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json"
 
-initCookiePopup("popup1", "vendors__list", "popup1-accept", "popup1-reject", path)
+function init() {
+    const cookieValue = getCookie("GDPR").split(';')[0]
+    if (!cookieValue || cookieValue === "Reject") {
+        initCookiePopup("popup1", "vendors__list", "popup1-accept", "popup1-reject", path)
+    }
+}
+
+init()
+
